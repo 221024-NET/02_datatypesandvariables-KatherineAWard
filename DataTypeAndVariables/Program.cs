6fa52f69fc74f03bc8cc7caa0be720b86f7cd6ad
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2022-10-27 14:53:12 -0700

    Pass Challenge

--- DataTypeAndVariables/Program.cs
+++ DataTypeAndVariables/Program.cs
@@ -8,22 +8,35 @@ namespace DataTypeAndVariables
       {
           Console.WriteLine("Hello World!");
 
-          byte myByte;
-          sbyte mySbyte;
-          int myInt;
-          uint myUint;
-          short myShort;
-          ushort myUShort;
-          float myFloat;
-          double myDouble;
-          char myCharacter;
-          bool myBool;
-          string myText;
-          string numText;
+          byte myByte = 134;
+          sbyte mySbyte = -125;
+          int myInt = 24558;
+          uint myUint = 54889;
+          short myShort = 4252;
+          ushort myUShort = 25485;
+          float myFloat = 33.45f;
+          double myDouble = 54.56452;
+          char myCharacter = 'k';
+          bool myBool = true;
+          string myText = "Hello World";
+          string numText = "937";
+
+          string stringVar = "I control text";
+          string numVar = "47";
+
+          Console.WriteLine(stringVar);
+          Console.WriteLine(Text2Num(numVar));
+
       }
 
       public static int Text2Num(string numText)
       {
+        int success;
+        if (Int32.TryParse(numText, out success))
+        {
+          return success;
+        }
+
         throw new NotImplementedException();
       }
     }
